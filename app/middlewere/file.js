b9f3708a2e46bec8be205fb1c6c7356350e4f629
Kukhmirov Vitaly <80460387+Kukhmirov@users.noreply.github.com>
2023-02-27 21:13:54 +0300

    create upload and validation file, add logs

---
+++ app/middlewere/file.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination(req, res, callback){
+        callback(null, 'app/assets/books');
+    },
+    filename(req, file, callback){
+        callback(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+const fileFilter = (req, file, callback) => {
+    if(file.mimetype === 'text/plain') {
+        callback(null, true);
+    }
+    else{
+        callback(null, false);
+    }
+};
+
+module.exports = multer({storage, fileFilter});
