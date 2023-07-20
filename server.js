cf6c5a266e76b8c83ef2991d91b392807c7cf08c
Kukhmirov Vitaly <v.kukhmirov@just-work.org>
2023-07-20 17:16:13 +0300

    dockerfile

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(logger);
 require("./app/routers/router")(app);
 app.use(err404);
 
-const PORT = process.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
